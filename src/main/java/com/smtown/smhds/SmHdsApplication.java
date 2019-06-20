54966ed5e47150873b9a3acecacc2fe2cf85a8a1
shaorin62 <shaorin62@gmail.com>
2019-06-20 18:40:07 +0900

    initsource

---
+++ src/main/java/com/smtown/smhds/SmHdsApplication.java
@@ -0,0 +1,29 @@
+package com.smtown.smhds;
+
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+@MapperScan(basePackages = {"com.smtown.smhds.*"})
+public class SmHdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmHdsApplication.class, args);
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+
+        sessionFactory.setDataSource(dataSource);
+        return sessionFactory.getObject();
+    }
+
+}
